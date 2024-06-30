7015c977cfdfa61c0a93f595052618f062b7b22f
sanasyt1403 <sanasyt1403@gmail.com>
2024-07-01 02:04:46 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -20,3 +20,18 @@ $(document).ready(function(){
 
     toggleCard('#toggleButton3', '#thirdCard');
 });
+
+function getGreeting() {
+    const currentDate = new Date();
+    const currentHour = currentDate.getHours();
+
+    if (currentHour < 12) {
+        return "Good Morning! Sana okay ka pa :))";
+    } else if (currentHour < 18) {
+        return "Good Afternoon!";
+    } else {
+        return "Good Evening!";
+    }
+}
+
+document.getElementById("greet").innerHTML = getGreeting();
